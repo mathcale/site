f6437bb8e331f36354ad01654594ca0e53cb38b6
Matheus Calegaro <matheuscalegaro@outlook.com>
2021-04-30 00:57:57 -0300

    chore(tailwind): add new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins'],
+        cursive: ['"Homemade Apple"']
+      }
+    },
   },
   variants: {
     extend: {},
