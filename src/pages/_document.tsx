13522c52ae111ecf3dc487aa3e15468c3172e5aa
Matheus Calegaro <hello@matheus.me>
2021-08-03 01:23:05 -0300

    chore: configuring custom font on Document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
