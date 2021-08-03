84269f50e36e345286aaa8655d05e78857e4c677
Matheus Calegaro <hello@matheus.me>
2021-08-03 01:23:40 -0300

    chore: applying custom font, disabling core container plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [require('tailwind-bootstrap-grid')()],
   corePlugins: {
-   container: false,
-  }
-}
+    container: false,
+  },
+};
