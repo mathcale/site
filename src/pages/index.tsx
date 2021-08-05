0a6ffd06fbae563697cad0077d5bae8a8bf1515d
Matheus Calegaro <hello@matheus.me>
2021-08-05 01:27:40 -0300

    feat(pages): implementing Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,63 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { Layout } from '../components';
+
+import pic from '../assets/img/mc.jpg';
+import styles from '../assets/css/index.module.css';
 
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="flex h-screen">
+        <div className="row m-auto">
+          <div className="md:col-6 flex justify-center relative">
+            <img src="/img/dots.svg" width={77} height={77} className="absolute -top-7 right-20" />
+
+            <img
+              src="/img/dots.svg"
+              width={77}
+              height={77}
+              className="absolute -bottom-7 left-20"
+            />
+
+            <Image src={pic} width={520} height={520} placeholder="blur" className={styles.pic} />
+          </div>
+
+          <div className="md:col-5 flex flex-col justify-center">
+            <h1 className="mb-16 text-6xl text-white font-extrabold leading-relaxed">
+              I’m a full-stack Software Engineer from Brazil
+            </h1>
+
+            <ul className={styles.socials}>
+              <li>
+                <a href="https://github.com/mathcale" title="Github">
+                  <Image src="/img/github.svg" width={32} height={32} />
+                </a>
+              </li>
+
+              <li>
+                <a href="https://linkedin.com/in/mathcale" title="LinkedIn">
+                  <Image src="/img/linkedin.svg" width={32} height={32} />
+                </a>
+              </li>
+
+              <li>
+                <a href="https://codepen.io/mathcale" title="Codepen">
+                  <Image src="/img/codepen.svg" width={32} height={32} />
+                </a>
+              </li>
+
+              <li>
+                <Link href="/contact">
+                  <a title="Contact">
+                    <Image src="/img/email.svg" width={32} height={32} />
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
+  );
 }
