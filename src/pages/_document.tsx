da11e37d9f087a86e32bfab502c506ecaf251be5
Matheus Calegaro <hello@matheus.me>
2021-08-05 01:19:29 -0300

    chore(fonts): importing correct weights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap"
+          />
         </Head>
 
         <body>
