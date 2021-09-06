6117953e5d5d5c3ef1a95f8e98adf0db796e250e
Matheus Calegaro <hello@matheus.me>
2021-09-05 23:10:44 -0300

    feat(components/navbar): making menu work on mobile viewport

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Transition } from '@headlessui/react';
 
 interface NavbarProps {
   absolute: boolean;
@@ -34,105 +33,49 @@ export function Navbar({ absolute }: NavbarProps) {
       </Link>
     ));
 
-  const renderMobileNavLinks = (): JSX.Element[] =>
-    navLinks.map((navLink, i) => (
-      <Link href={navLink.href} key={i}>
-        <a
-          className={
-            router.pathname === navLink.href
-              ? 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-          }
-        >
-          {navLink.title}
-        </a>
-      </Link>
-    ));
-
   return (
-    <nav className={['bg-transparent', 'w-full', absolute && 'absolute'].join(' ')}>
+    <nav className={['bg-transparent z-10', absolute ? 'md:absolute w-full' : ''].join(' ')}>
       <div className="container">
-        <div className="relative flex items-center justify-between h-20">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div className="flex flex-col md:items-center md:justify-between md:flex-row">
+          <div className="flex flex-row items-center justify-between h-20">
+            <Link href="/">
+              <a className="flex items-center ml-3 md:ml-0">
+                <Image src="/img/logo.svg" alt="Logo" width={32} height={32} />
+                <h1 className="text-white font-bold text-xl ml-4">Matheus Calegaro</h1>
+              </a>
+            </Link>
+
             <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
+              className="md:hidden focus:shadow-outline inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               onClick={() => setIsOpen(!isOpen)}
             >
-              <span className="sr-only">Open main menu</span>
-
-              {!isOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
+              <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
+                {isOpen ? (
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                ) : (
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              )}
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  ></path>
+                )}
+              </svg>
             </button>
           </div>
 
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
-              <Link href="/">
-                <a className="flex items-center">
-                  <Image src="/img/logo.svg" alt="Logo" width={32} height={32} />
-                  <h1 className="text-white font-bold text-xl ml-4">Matheus Calegaro</h1>
-                </a>
-              </Link>
-            </div>
-          </div>
-
-          <Transition
-            show={isOpen}
-            enter="transition ease-out duration-100 transform"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="transition ease-in duration-75 transform"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
+          <div
+            className={[
+              isOpen ? 'flex' : 'hidden',
+              'flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row',
+            ].join(' ')}
           >
-            {(ref) => (
-              <div
-                ref={ref}
-                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 md:hidden"
-              >
-                <div className="flex space-x-4">{renderNavLinks()}</div>
-              </div>
-            )}
-          </Transition>
+            {renderNavLinks()}
+          </div>
         </div>
-      </div>
-
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1">{renderMobileNavLinks()}</div>
       </div>
     </nav>
   );
