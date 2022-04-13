618b0e3c678e9be1861b5473d5b5f5cf5f1bfeaa
Matheus Calegaro <hello@matheus.me>
2022-04-13 00:19:24 -0300

    chore(blog/post): add page title, WIP styling parsed markdown

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
+import Head from 'next/head';
 
 import { Layout } from '../../components';
 
@@ -9,15 +10,30 @@ const POSTS_PATH = join(process.cwd(), 'posts');
 
 export default function BlogPostPage(props) {
   return (
-    <Layout>
-      <article>
-        <h1>{props.frontmatter.title}</h1>
+    <>
+      <Head>
+        <title>{props.frontmatter.title} - Blog - Matheus Calegaro</title>
+      </Head>
 
-        <div>
-          <ReactMarkdown children={props.markdownBody} />
-        </div>
-      </article>
-    </Layout>
+      <Layout>
+        <article className="mt-7">
+          <h1 className="my-12 text-5xl md:text-6xl text-white font-extrabold">
+            {props.frontmatter.title}
+          </h1>
+
+          <div>
+            <ReactMarkdown
+              children={props.markdownBody}
+              components={{
+                p({ children, ...props }) {
+                  return <p className="text-white">{children}</p>;
+                },
+              }}
+            />
+          </div>
+        </article>
+      </Layout>
+    </>
   );
 }
 
