850ef049b5cb0339b2df9495117877353224bff9
Matheus Calegaro <hello@matheus.me>
2026-08-25 17:28:21 -0300

    chore: reset codebase

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
