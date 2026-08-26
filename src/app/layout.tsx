616aabf82305a1b19f4d3a99350c430dd65420bf
Matheus Calegaro <hello@matheus.me>
2026-08-25 22:25:21 -0300

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Fira_Code } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 import type { Metadata } from 'next';
 
@@ -13,12 +14,16 @@ export const metadata: Metadata = {
   title: 'Matheus Calegaro',
 };
 
+const GA_TRACKING_ID = 'G-GLF02KY1PX';
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={firaCode.className}>
       <body>
         <main className="main-layout">{children}</main>
       </body>
+
+      <GoogleAnalytics gaId={GA_TRACKING_ID} />
     </html>
   );
 }
